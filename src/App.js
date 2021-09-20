f1bc189f5dd294a15215251a055bfa4fcd91290c
Nirodha97 <nirodhamwickramaratne@gmail.com>
2021-09-20 11:40:16 +0530

    styling patterns

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-
-function App() {
+import React, {Component} from 'react';
+import './App.css'
+class App extends Component {
   
-const name ="Nirodha"
-  return (
-    <div className="App">
-     react {name}
-    </div>
-  );
+
+  render(){
+    const styles={
+      border:'solid',
+      textAlign:'center',
+      boxShadow:'2px 2px'
+    }
+    return (
+      <div className="App">
+      <div style={styles}>Style
+      </div>
+      <button>Button 1 </button>
+      react {this.name} 
+
+      </div>
+    );
+  } 
 }
 
+
+const Appinstanse = new App();
+console.log(Appinstanse.name)
 export default App;
